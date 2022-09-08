88372d0fdecc29b78301eaebfc92cc4f60f0a40a
DartElina <109183572+DartElina@users.noreply.github.com>
2022-09-07 21:38:28 -0400

    Add files via upload

--- Pewlett-Hackard-Analysis./Pewlett-Hackard-Analysis./Employee_Database_challenge.sql
+++ Pewlett-Hackard-Analysis./Pewlett-Hackard-Analysis./Employee_Database_challenge.sql
@@ -15,11 +15,22 @@ From retirement_titles as rt
 WHERE (to_date = '9999-01-01')
 ORDER BY emp_no ASC;
 -- number of employees by their most recent title who are about to retire
-SELECT COUNT(emp_no), title
+SELECT COUNT(title), title
+INTO retiring_titles
 FROM unique_titles
+Group by title
+order by count(title) DESC;
 
--- SELECT column_name(s)
--- FROM table_name
--- WHERE condition
--- GROUP BY column_name(s)
--- ORDER BY column_name(s);
+-- creating a mentor eligibility list of employees
+SELECT DISTINCT ON (emp_no) e.emp_no, e.first_name, e.last_name, e.birth_date, 
+    de.from_date, de.to_date
+    t.title
+INTO mentorship_eligible
+FROM employees as e 
+Inner Join dept_emp as de 
+ON e.emp_no = de.emp_no
+INNER JOIN titles as t
+ON e.emp_no = t.emp_no
+WHERE (e.birth_date BETWEEN '1965-01-01'AND '1965-12-31')
+AND (de.to_date = '9999-01-01')
+ORDER BY e.emp_no;
